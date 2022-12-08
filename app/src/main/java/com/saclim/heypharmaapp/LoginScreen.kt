f1a03a8421aede58d5c7f60f98dcd907e5b43aa9
Sajana <navodyasajana234@gmail.com>
2022-12-09 02:33:05 +0530

    login done version 1.3

--- app/src/main/java/com/saclim/heypharmaapp/LoginScreen.kt
+++ app/src/main/java/com/saclim/heypharmaapp/LoginScreen.kt
@@ -1,21 +1,83 @@
 package com.saclim.heypharmaapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 import com.saclim.heypharmaapp.R
 import kotlinx.android.synthetic.main.screen_login.*
 
+
+
 class LoginScreen : AppCompatActivity() {
+
+    private lateinit var firebaseAuth:FirebaseAuth
+
+    private lateinit var loginUsername:TextInputEditText
+    private lateinit var loginPassword:TextInputEditText
+
+    private lateinit var progressDialog:ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.screen_login)
 
 
-        lblLoginRegister.setOnClickListener(){
 
+        loginUsername = findViewById(R.id.loginUsername)
+        loginPassword = findViewById(R.id.loginPassword)
+
+        clearErrorMessages()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please Wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        lblLoginRegister.setOnClickListener(){
+            clearErrorMessages()
             val intent = Intent(this, RegistrationScreen::class.java)
             startActivity(intent)
         }
+
+        btnLoginLogin.setOnClickListener{
+            try{
+                val email=loginUsername.text.toString()
+                val pass=loginPassword.text.toString()
+
+                if(email.isNotEmpty() && pass.isNotEmpty()){
+                    clearErrorMessages()
+                    progressDialog.setMessage("Logging...!")
+                    progressDialog.show()
+                    firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener { result->
+                        if(result.isSuccessful){
+                            progressDialog.dismiss()
+                            Toast.makeText(this,"Login Success",Toast.LENGTH_SHORT).show()
+
+                        }else{
+                            progressDialog.dismiss()
+                            Toast.makeText(this,result.exception.toString(),Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }else{
+                    clearErrorMessages()
+                    progressDialog.dismiss()
+                    if(email.isNullOrEmpty()) {
+                        textInputUsername.setHelperText("*Enter Email...")
+                    }else if(pass.isNullOrEmpty()) {
+                        textInputPassword.setHelperText("*Enter Password...")
+                    }
+                }
+            }catch(e:Exception){
+                Toast.makeText(this,e.message.toString(),Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun clearErrorMessages(){
+        textInputUsername.setHelperText("")
+        textInputPassword.setHelperText("")
     }
 }
