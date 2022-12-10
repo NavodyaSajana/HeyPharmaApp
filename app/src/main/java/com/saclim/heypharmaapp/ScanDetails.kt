d26b9a862cbf449926def6137d9642c121ba66f3
Sajana <navodyasajana234@gmail.com>
2022-12-11 03:49:09 +0530

    pharmacy finder done...! ver 1.0

--- app/src/main/java/com/saclim/heypharmaapp/ScanDetails.kt
+++ app/src/main/java/com/saclim/heypharmaapp/ScanDetails.kt
@@ -1,27 +1,36 @@
 package com.saclim.heypharmaapp
 
+import android.app.AlertDialog
 import android.app.ProgressDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.button.MaterialButton
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_scan_here.*
-import org.json.JSONObject
+import kotlinx.android.synthetic.main.available_pharmacy_recycler_item.*
+
 
 private lateinit var txtMedication:TextView
 private lateinit var txtDiciese:TextView
 private lateinit var txtMedDescription:TextView
 private lateinit var medImg:ImageView
 private lateinit var btnScanDetailsCancel:MaterialButton
+private lateinit var scanDetailsRecycle:RecyclerView
+private lateinit var pharmacyList:ArrayList<Pharmacy>
 
-private lateinit var drugDetailsObject:JSONObject
 private lateinit var firebaseDatabase: FirebaseDatabase
 private lateinit var databaseReference: DatabaseReference
 
@@ -44,6 +53,13 @@ class ScanDetails : AppCompatActivity() {
         medImg = findViewById(R.id.medImg)
         btnScanDetailsCancel = findViewById(R.id.btnScanDetailsCancel)
 
+
+        scanDetailsRecycle = findViewById(R.id.ScanDetailsRecycle)
+        scanDetailsRecycle.adapter=pharmacyAdapter()
+        scanDetailsRecycle.layoutManager = LinearLayoutManager(this)
+        scanDetailsRecycle.setHasFixedSize(true)
+        pharmacyList = arrayListOf<Pharmacy>()
+
         progressDialog = ProgressDialog(this)
         progressDialog.setTitle("Please Wait")
         progressDialog.setCanceledOnTouchOutside(false)
@@ -52,7 +68,7 @@ class ScanDetails : AppCompatActivity() {
             if(bundle.getString("medicationName")!=null){
                 val medicationName = bundle.getString("medicationName").toString()
                 makeToast(medicationName)
-                txtMedication.setText(medicationName)
+                txtMedication.text = medicationName
                 setDetails(medicationName)
             }
         }
@@ -101,16 +117,134 @@ class ScanDetails : AppCompatActivity() {
         databaseReference = firebaseDatabase.getReference("Drug")
 
         databaseReference.child(drugName).get().addOnSuccessListener{ result->
-        txtDiciese.setText("Disease : ${result.child("Disease").value.toString()}")
-        txtMedDescription.setText(result.child("Description").value.toString())
-        val imageUrl = result.child("Image").value.toString()
-        Glide.with(applicationContext)
-            .load(imageUrl)
-            .override(130, 130)
-            .into(medImg)
+            txtDiciese.text = "Disease : ${result.child("Disease").value.toString()}"
+            txtMedDescription.text = result.child("Description").value.toString()
+            val imageUrl = result.child("Image").value.toString()
+            Glide.with(applicationContext)
+                .load(imageUrl)
+                .override(130, 130)
+                .into(medImg)
+            setPharmacyDetails(drugName)
             progressDialog.dismiss()
         }
     }
+
+    private fun setPharmacyDetails(drugName:String) {
+        var pharmacyNumbers = ArrayList<String>()
+        progressDialog.setMessage("Pharmacy Details Loading...")
+        progressDialog.show()
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.getReference("Drug/${drugName}/AvailablePharmacy")
+
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(pharmaSnapshot in snapshot.children){
+                    val data = pharmaSnapshot.getValue().toString()
+                    pharmacyNumbers.add(data)
+                    //makeToast(data)
+                }
+                loadPharmacyDetails(pharmacyNumbers)
+                progressDialog.dismiss()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                makeToast("Unable to find Pharmacy")
+                progressDialog.dismiss()
+            }
+        })
+    }
+    private fun loadPharmacyDetails(pharmacyNumbers:ArrayList<String>){
+        progressDialog.setMessage("Pharmacy Details Loading...")
+        progressDialog.show()
+        val end = pharmacyNumbers.size-1
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Pharmacy")
+
+        databaseReference.addValueEventListener(object : ValueEventListener{
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                pharmacyList.clear()
+                if(snapshot.exists()){
+                    for(pharmacySnapshot in snapshot.children){
+                        val pharmacy = pharmacySnapshot.getValue(Pharmacy::class.java)
+
+                        for(c in 0..end){
+                            if(pharmacy!!.telephone.toString()==pharmacyNumbers.get(c)){
+                                pharmacyList.add(pharmacy!!)
+                            }
+                        }
+                    }
+                    scanDetailsRecycle.adapter?.notifyDataSetChanged()
+                    progressDialog.dismiss()
+                }else{
+                    progressDialog.dismiss()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                progressDialog.dismiss()
+                makeToast("Data is not available go back and try again")
+            }
+        })
+
+    }
+    private inner class pharmacyAdapter : RecyclerView.Adapter<PharmacyViewHolder>(){
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PharmacyViewHolder {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.available_pharmacy_recycler_item,
+            parent,false)
+
+            return PharmacyViewHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: PharmacyViewHolder, position: Int) {
+
+            val currentItem = pharmacyList[position]
+            holder.txtRePharmaName.text=currentItem.Name
+            holder.txtRePharmaTP.text=currentItem.telephone
+            holder.txtRePharmaLocation.text=currentItem.Location
+            Glide.with(applicationContext)
+                .load(currentItem.ImagePharmacy)
+                .override(100, 70)
+                .into(holder.imgPharmacy)
+            holder.cardView.setOnClickListener{
+                val selectedItem = pharmacyList[position]
+
+
+                /*val confirmDialog = AlertDialog.Builder(applicationContext)
+                confirmDialog.setTitle("Confirm")
+                confirmDialog.setMessage("Are you sure to Contact ${selectedItem.Name.toString()}")
+                confirmDialog.setIcon(android.R.drawable.button_onoff_indicator_on)
+                confirmDialog.setPositiveButton("Yes"){dialogInterface,which->
+                    makeToast(selectedItem.Name.toString())
+                }
+                confirmDialog.setNeutralButton("Cancel"){dialogInterface,which->
+                    makeToast("Cancelled")
+                }
+                confirmDialog.setNegativeButton("No"){dialogInterface,which->
+                    makeToast("Select More...!")
+                }
+
+                val alertDialog:AlertDialog = confirmDialog.create()
+                alertDialog.setCancelable(false)
+                alertDialog.show()*/
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return pharmacyList.size
+        }
+
+    }
+    private inner class PharmacyViewHolder(itemView : View):RecyclerView.ViewHolder(itemView){
+        val txtRePharmaName:TextView = itemView.findViewById(R.id.txtRePharmaName)
+        val txtRePharmaTP:TextView = itemView.findViewById(R.id.txtRePharmaTP)
+        val txtRePharmaLocation:TextView = itemView.findViewById(R.id.txtRePharmaLocation)
+        val imgPharmacy:ImageView = itemView.findViewById(R.id.imgPharmacy)
+        val cardView : CardView = itemView.findViewById(R.id.main_tab)
+
+    }
+
     private fun makeToast(value:String){
         Toast.makeText(this,value,Toast.LENGTH_SHORT).show()
     }
